88b3c9bd6ac635557be17e18cf877447f75787dc
Devesh Lohia <devesh@iastate.edu>
2023-09-12 12:44:53 -0500

    first-push

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+import uvicorn
 from fastapi import FastAPI, File, UploadFile
 import boto3
 from botocore.exceptions import NoCredentialsError
 from dotenv import load_dotenv
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
 app = FastAPI()
 
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Replace these with your DigitalOcean Spaces credentials
 ACCESS_KEY = os.getenv('ACCESS_KEY')
@@ -34,3 +45,6 @@ async def upload_file(file: UploadFile):
         return {"message": "File uploaded successfully"}
     except NoCredentialsError:
         return {"error": "No AWS credentials found"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
